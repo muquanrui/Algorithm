54fb6c27234fd76495c1e39ff1fb6ab4440e0468
muquanrui <muquanrui@gmail.com>
2022-02-19 23:43:22 +0100

    Commit 2022.2.19

--- leetcode/src/ink/vor/binarytree/LeetCode0094.java
+++ leetcode/src/ink/vor/binarytree/LeetCode0094.java
@@ -2,30 +2,47 @@ package ink.vor.binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author muquanrui
  * @date 18/02/2022 22:45
  */
 public class LeetCode0094 {
-    List<Integer> resList;
-
+    // List<Integer> resList;
+    //
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     resList = new ArrayList<>();
+    //     if (root == null) {
+    //         return resList;
+    //     }
+    //     recursiveTraversal(root);
+    //     return resList;
+    // }
+    //
+    // private void recursiveTraversal(TreeNode node) {
+    //     if (node.left != null) {
+    //         recursiveTraversal(node.left);
+    //     }
+    //     resList.add(node.val);
+    //     if (node.right != null) {
+    //         recursiveTraversal(node.right);
+    //     }
+    // }
     public List<Integer> inorderTraversal(TreeNode root) {
-        resList = new ArrayList<>();
-        if (root == null) {
-            return resList;
+        List<Integer> resList = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()){
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                resList.add(cur.val);
+                cur = cur.right;
+            }
         }
-        recursiveTraversal(root);
         return resList;
-    }
-
-    private void recursiveTraversal(TreeNode node) {
-        if (node.left != null) {
-            recursiveTraversal(node.left);
-        }
-        resList.add(node.val);
-        if (node.right != null) {
-            recursiveTraversal(node.right);
-        }
     }
 }
--- leetcode/src/ink/vor/binarytree/LeetCode0144.java
+++ leetcode/src/ink/vor/binarytree/LeetCode0144.java
@@ -2,29 +2,49 @@ package ink.vor.binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author muquanrui
  * @date 18/02/2022 22:36
  */
 public class LeetCode0144 {
-    List<Integer> resList;
+    // List<Integer> resList;
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     resList = new ArrayList<>();
+    //     if (root == null) {
+    //         return resList;
+    //     }
+    //     recursiveTraversal(root);
+    //     return resList;
+    // }
+    //
+    // private void recursiveTraversal(TreeNode node) {
+    //     resList.add(node.val);
+    //     if (node.left != null) {
+    //         recursiveTraversal(node.left);
+    //     }
+    //     if (node.right != null) {
+    //         recursiveTraversal(node.right);
+    //     }
+    // }
     public List<Integer> preorderTraversal(TreeNode root) {
-        resList = new ArrayList<>();
-        if (root == null) {
-            return resList;
+        List<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
         }
-        recursiveTraversal(root);
-        return resList;
-    }
-
-    private void recursiveTraversal(TreeNode node) {
-        resList.add(node.val);
-        if (node.left != null) {
-            recursiveTraversal(node.left);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null){
+                stack.push(node.right);
+            }
+            if (node.left != null){
+                stack.push(node.left);
+            }
         }
-        if (node.right != null) {
-            recursiveTraversal(node.right);
-        }
+        return result;
     }
 }
