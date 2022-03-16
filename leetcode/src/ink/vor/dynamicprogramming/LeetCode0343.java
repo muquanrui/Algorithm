94b2e8773667cd840cab7eb621b84bd2ceaa84fb
muquanrui <muquanrui@gmail.com>
2022-03-16 23:33:01 +0100

    Commit 2022.3.16

---
+++ leetcode/src/ink/vor/dynamicprogramming/LeetCode0343.java
@@ -0,0 +1,19 @@
+package ink.vor.dynamicprogramming;
+
+/**
+ * @author muquanrui
+ * @date 16/03/2022 23:18
+ */
+public class LeetCode0343 {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
