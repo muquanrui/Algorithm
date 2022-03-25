4cd654d18057d3ab947b43c981dded555e8b9961
muquanrui <muquanrui@gmail.com>
2022-03-25 18:27:50 +0100

    Commit 2022.3.25

---
+++ leetcode/src/ink/vor/dynamicprogramming/LeetCode0718.java
@@ -0,0 +1,21 @@
+package ink.vor.dynamicprogramming;
+
+/**
+ * @author muquanrui
+ * @date 25/03/2022 17:39
+ */
+public class LeetCode0718 {
+    public int findLength(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int res = 0;
+        for (int i = 1; i < nums1.length + 1; i++) {
+            for (int j = 1; j < nums2.length + 1; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+}
