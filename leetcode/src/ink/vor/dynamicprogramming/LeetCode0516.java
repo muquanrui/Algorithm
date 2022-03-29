1683aad0e179a9cf048f88d0724c2395787e7641
muquanrui <muquanrui@gmail.com>
2022-03-29 23:18:53 +0200

    Commit 2022.3.28

---
+++ leetcode/src/ink/vor/dynamicprogramming/LeetCode0516.java
@@ -0,0 +1,25 @@
+package ink.vor.dynamicprogramming;
+
+/**
+ * @author muquanrui
+ * @date 29/03/2022 23:09
+ */
+public class LeetCode0516 {
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+        }
+        int max = 1;
+        for (int i = s.length() - 1; i > -1; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
+}
