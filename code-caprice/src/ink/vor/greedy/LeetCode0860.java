b6c7f2769323073b7a0ab117ddc4e168fb8d0358
muquanrui <muquanrui@gmail.com>
2022-05-10 23:55:30 +0800

    Commit 2022.5.10

---
+++ code-caprice/src/ink/vor/greedy/LeetCode0860.java
@@ -0,0 +1,31 @@
+package ink.vor.greedy;
+
+/**
+ * @author muquanrui
+ * @date 2022/5/10 23:55
+ */
+public class LeetCode0860 {
+    public boolean lemonadeChange(int[] bills) {
+        int count5 = 0;
+        int count10 = 0;
+        for (int i : bills) {
+            if (i == 5) {
+                count5++;
+            } else if (i == 10) {
+                count5--;
+                count10++;
+            } else {
+                if (count10 > 0) {
+                    count10--;
+                    count5--;
+                } else {
+                    count5 -= 3;
+                }
+            }
+            if (count5 < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
