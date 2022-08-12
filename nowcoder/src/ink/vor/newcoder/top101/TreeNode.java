10cdb06adf5130199927131470736300f891cc57
muquanrui <muquanrui@gmail.com>
2022-08-12 21:53:47 +0800

    Commit 2022.8.12

--- nowcoder/src/ink/vor/newcoder/top101/TreeNode.java
+++ nowcoder/src/ink/vor/newcoder/top101/TreeNode.java
@@ -5,9 +5,9 @@ package ink.vor.newcoder.top101;
  * @date 04/04/2022 17:01
  */
 public class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
     public TreeNode(int val) {
         this.val = val;
     }
