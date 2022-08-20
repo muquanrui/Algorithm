acba8cc3785808e1720dd1a13bc487aac7334c6a
muquanrui <muquanrui@gmail.com>
2022-08-20 13:54:30 +0800

    Commit 2022.8.20

---
+++ nowcoder/src/ink/vor/newcoder/test/Test04.java
@@ -0,0 +1,26 @@
+package ink.vor.newcoder.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * @author muquanrui
+ * @date 2022/8/20 13:29
+ */
+public class Test04 {
+    public static void main(String[] args) {
+        try ( BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            // int count = Integer.parseInt(br.readLine());
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] strs = line.split(" ");
+                int i1 = Integer.parseInt(strs[0]);
+                int i2 = Integer.parseInt(strs[1]);
+                System.out.println(i1 + i2);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
