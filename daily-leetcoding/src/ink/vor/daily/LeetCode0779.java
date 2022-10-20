542381d7b2a62ceeffa68474201bc5181f1edba4
muquanrui <muquanrui@gmail.com>
2022-10-20 22:31:39 +0800

    Commit 2022.10.20

---
+++ daily-leetcoding/src/ink/vor/daily/LeetCode0779.java
@@ -0,0 +1,20 @@
+package ink.vor.daily;
+
+/**
+ * @author muquanrui
+ * @date 2022/10/20 22:01
+ */
+
+public class LeetCode0779 {
+    public int kthGrammar(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        int last = kthGrammar(n - 1, (k + 1) / 2);
+        if (k % 2 == 1) {
+            return last;
+        } else {
+            return last ^ 1;
+        }
+    }
+}
