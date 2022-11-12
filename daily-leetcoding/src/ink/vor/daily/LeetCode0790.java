f314ab2c617eb9b1bd292da292ab556761bda73b
muquanrui <muquanrui@gmail.com>
2022-11-12 13:52:29 +0800

    Commit 2022.11.12

---
+++ daily-leetcoding/src/ink/vor/daily/LeetCode0790.java
@@ -0,0 +1,22 @@
+package ink.vor.daily;
+
+/**
+ * @author muquanrui
+ * @date 2022/11/12 12:08
+ */
+
+public class LeetCode0790 {
+    public int numTilings(int n) {
+        final int MOD = 1000000007;
+        int[][] dp = new int[n][4];
+        dp[0][0] = 1;
+        dp[0][3] = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][3];
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            dp[i][3] = (((dp[i - 1][0] + dp[i - 1][1]) % MOD + dp[i - 1][2]) % MOD + dp[i - 1][3]) % MOD;
+        }
+        return dp[n - 1][3];
+    }
+}
