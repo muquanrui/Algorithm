d7c1f658cab1fbe1a1da1c2304d909f136998e89
muquanrui <muquanrui@gmail.com>
2022-11-27 02:55:20 +0800

    Commit 2022.11.27

---
+++ daily-leetcoding/src/ink/vor/daily/LeetCode1752.java
@@ -0,0 +1,26 @@
+package ink.vor.daily;
+
+/**
+ * @author muquanrui
+ * @date 2022/11/27 02:30
+ */
+
+// https://leetcode.cn/problems/check-if-array-is-sorted-and-rotated/
+public class LeetCode1752 {
+    public boolean check(int[] nums) {
+        boolean flag = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                if (!flag) {
+                    flag = true;
+                } else {
+                    return false;
+                }
+            }
+            if (flag && nums[i] > nums[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
