38c73f1772de3b65aefb1fe52f1887bef4c7c8a7
muquanrui <muquanrui@gmail.com>
2022-12-11 16:50:22 +0800

    Commit 2022.12.11

---
+++ daily-leetcoding/src/ink/vor/daily/LeetCode1827.java
@@ -0,0 +1,20 @@
+package ink.vor.daily;
+
+/**
+ * @author muquanrui
+ * @date 2022/12/11 05:16
+ */
+
+// https://leetcode.cn/problems/minimum-operations-to-make-the-array-increasing/
+public class LeetCode1827 {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                count += nums[i - 1] + 1 - nums[i];
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return count;
+    }
+}
