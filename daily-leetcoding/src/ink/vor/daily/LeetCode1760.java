c4525b8763cd1d6ffc91772e1d2156370e94aa01
muquanrui <muquanrui@gmail.com>
2022-12-20 16:17:51 +0800

    Commit 2022.12.20

---
+++ daily-leetcoding/src/ink/vor/daily/LeetCode1760.java
@@ -0,0 +1,29 @@
+package ink.vor.daily;
+
+import java.util.Arrays;
+
+/**
+ * @author muquanrui
+ * @date 2022/12/20 16:12
+ */
+
+public class LeetCode1760 {
+    public int minimumSize(int[] nums, int maxOperations) {
+        int max = Arrays.stream(nums).max().getAsInt();
+        int left = 1;
+        int right = max;
+        while (left <= right) {
+            int target = left + (right - left) / 2;
+            long ops = 0;
+            for (int n : nums) {
+                ops += (n - 1) / target;
+            }
+            if (ops > maxOperations) {
+                left = target + 1;
+            } else {
+                right = target - 1;
+            }
+        }
+        return left;
+    }
+}
