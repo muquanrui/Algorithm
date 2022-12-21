7686d1b02cf93ad9bc3515a707f2f21fbbecce0e
muquanrui <muquanrui@gmail.com>
2022-12-21 17:30:26 +0800

    Commit 2022.12.21

---
+++ daily-leetcoding/src/ink/vor/daily/LeetCode1753.java
@@ -0,0 +1,14 @@
+package ink.vor.daily;
+
+/**
+ * @author muquanrui
+ * @date 2022/12/21 01:39
+ */
+
+public class LeetCode1753 {
+    public int maximumScore(int a, int b, int c) {
+        int sum = a + b + c;
+        int maxVal = Math.max(Math.max(a, b), c);
+        return Math.min(sum - maxVal, sum / 2);
+    }
+}
